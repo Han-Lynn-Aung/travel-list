8f980447211bc65f478b9baa61d31a696900e40d
Han-Lynn-Aung <hanlynnaung1997@gmail.com>
2023-11-14 23:15:47 +0630

    sorted and project done

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItetms] = useState([]);
+
+  function handleAddItems(item) {
+    setItetms((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItetms((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItetms((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confrimed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confrimed) {
+      setItetms([]);
+    }
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
